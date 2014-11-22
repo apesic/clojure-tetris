5ec6b029c2ae19bf5f90dde29af26daef38fde16
Alexei Pesic <aly.pesic@gmail.com>
2014-11-22 15:31:57 -0800

    Collision detection, gravity, and piece anchoring implemented.

--- src/clojure_tetris/core.clj
+++ src/clojure_tetris/core.clj
@@ -2,21 +2,6 @@
   (:require [lanterna.screen :as s])
   (:gen-class))
 
-;; BOARD
-(def COLS 10)
-(def ROWS 20)
-
-(defn blank-board []
-  (vec (take (* COLS ROWS) (repeat 0))))
-
-(defn position->xy [position]
-  (let [x (mod position COLS)
-        y (int (/ position COLS))]
-     [x y]))
-
-(defn xy->position [[x y]]
-  (+ x (* y COLS)))
-
 ;; BLOCKS
 (def square-block [[1 1]
                    [1 1]])
@@ -39,21 +24,66 @@
               [1 1 1]])
 
 (def j-block [[1 1 1]
-	      [1 0 0]])
+              [1 0 0]])
 
 (def blocks [square-block line-block z-block s-block t-block l-block j-block])
 
 (defn get-block []
   {:shape (rand-nth blocks), :xy [5 0]})
 
+(def COLS 10)
+(def ROWS 20)
+
+(defn map-matrix
+  [f matrix]
+  (vec (map-indexed  (fn [y row]
+    (vec (map-indexed  (fn [x cell] (f cell x y)) row)))
+      matrix)))
+
+(defn position->xy [position]
+  (let [x (mod position COLS)
+        y (int (/ position COLS))]
+     [x y]))
+
+(defn xy->position [[x y]]
+  (+ x (* y COLS)))
+
+(defn block-coords [block]
+  "Return a list of coordinates for each filled cell of block, e.g. ((1 3) (2 3) (2 4) (2 5))"
+  (let [matrix (:shape block)
+        [x y] (:xy block)
+        coord-matrix (map-matrix (fn [cell cell-x cell-y]
+                       (vector (* cell (+ cell-x x)) (* cell (+ cell-y y)))) matrix)]
+        (filter #(not= '(0 0) %1) (partition 2 (flatten coord-matrix)))))
+
 ;; TODO: implement actual collision detection
+;; for each coord:
+;; not out of bounds (< 0, > COLS)
+;; not overlapping (board [x y] != 1)
+
 (defn collides?
   [board block]
-  false)
+  (let [block-coords (block-coords block)]
+    (not
+      (and
+        (every?
+          (fn [[x y]]
+            (< y ROWS))
+          block-coords)
+        (every?
+          (fn [[x y]]
+            (zero? (get board (xy->position [x y]))))
+          block-coords)
+        (every?
+          (fn [[x y]]
+            (and (>= x 0) (< x COLS)))
+          block-coords)))))
 
 (defn translate
   [dx dy board block]
   (let [translated (update-in block [:xy] #(vec (map + [dx dy] %)))]
+    (println board)
+    (println block)
     (if (collides? board translated)
       block
       translated)))
@@ -61,10 +91,32 @@
 ;;TODO: Refactor using comp
 (defn rotate
   [board block]
-  (let [rotated (update-in block [:shape] #(vec (apply map vector (map reverse %))))]
+  (let [rotated (update-in block [:shape]
+                           #(vec (apply map vector (map reverse %))))]
     (if (collides? board rotated)
       block
       rotated)))
+
+;; BOARD
+
+(defn blank-board []
+  (vec (take (* COLS ROWS) (repeat 0))))
+
+
+(defn merge-block
+  [board block-coords]
+  (if-let [[x y] (first block-coords)]
+    (recur (assoc board (xy->position [x y]) 1) (rest block-coords))
+    board))
+
+(defn down-or-anchor
+  [board block]
+  (let [test-block (translate 0 1 board block)]
+    (if (= block test-block)
+      [(merge-block board (block-coords block)) (get-block)]
+      [board test-block])))
+
+
 
 ;; SCREEN
 (def screen-width (+ COLS 8))
@@ -122,6 +174,14 @@
 
     :else nil))
 
+;; TODO:
+;; - Collision detection
+;; - Detect full lines
+;; - Clear lines
+;; - Game over?
+;; - Score
+;; - Increasing speed
+
 (defn -main
   [& args]
   (s/start scr)
@@ -129,7 +189,8 @@
   ;; Game Loop:
   (loop [board (blank-board)
          block (get-block)
-         score 0]
+         score 0
+         past-tick (System/currentTimeMillis)]
 
     ;; 1. Draw the board
     (clear-screen scr)
@@ -141,16 +202,34 @@
 
     ;; 2. Get user input
 
-    (if-let [action (key-mapping (s/get-key-blocking scr))]
-      (recur
-        board
-        (action board block)
-        score
-        )
-      (recur
-        board
-        block
-        score))
+    (let [action (key-mapping (s/get-key scr))
+          current-tick (System/currentTimeMillis)
+          next-tick (if (> (- current-tick past-tick) 600)
+                      current-tick
+                      past-tick)]
+      (if (> next-tick past-tick) (println "Down"))
+      (cond
+        (> next-tick past-tick)
+        (let [[new-board new-block] (down-or-anchor board block)]
+         (recur
+           new-board
+           new-block
+           score
+           next-tick))
+        action
+        (recur
+          board
+          (action board block)
+          score
+          next-tick)
+
+        :else
+        (recur
+          board
+          block
+          score
+          next-tick)))
+
     ;; 3. Process input, create new board
     ;; 4. Recur with new board
     ))
